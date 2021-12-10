d4615b0435e96c59d4d8daef06d25516909dc35b
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-10 23:23:55 +0300

    created serializer file

--- awwardsclone/settings.py
+++ awwardsclone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
     'awwards',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
