83f2b41eb009426496f9c14747ff95d49aabd516
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-10 23:42:40 +0300

    profile updates

--- awwards/urls.py
+++ awwards/urls.py
@@ -10,8 +10,16 @@ urlpatterns = [
     path('accounts/register/', views.registration, name='register'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('updateprofile/', views.updateprofile, name='updateprofile'),
+    path('new-project/', views.postproject, name='newproject'),
+    path('project/<id>', views.get_project, name='project'),
+    path('search/', views.search_projects, name='search_projects'),
+    path(r'ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('api/profile/', views.ProfileList.as_view(),name='profiles'),
+    path('api/projects/', views.ProjectsList.as_view(),name='projects'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    
--- awwards/views.py
+++ awwards/views.py
@@ -28,3 +28,54 @@ def registration(request):
     }
     return render(request, 'registration/register.html', context)
 
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    projects = Projects.objects.all()
+    posts = Profile.objects.all()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'posts':posts,
+    'projects':projects,
+    }
+    return render(request, 'profile/profile.html', context)
+
+
+@login_required(login_url='/accounts/login/')
+def updateprofile(request):
+    projects = Projects.objects.all()
+    posts = Profile.objects.all()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'posts':posts,
+    'projects':projects,
+    }
+
+    return render(request, 'profile/update_profile.html', context)
+
