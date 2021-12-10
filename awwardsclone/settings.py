47b66fe49a3d5a7d2d663c4a92df20bf35260c4c
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-10 22:35:57 +0300

    added on urls of app and projects

--- awwardsclone/settings.py
+++ awwardsclone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'awwards',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'awwardsclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awwards',
+        'USER': 'moringa',
+    'PASSWORD':'Access',
     }
 }
 
