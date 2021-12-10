4229ac33d80f7f9ec5b41afd6c09ee7ef73f37ae
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-10 22:28:13 +0300

    views import and login functions

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def home(request):
+    projects = Projects.objects.all()
+    context = {
+    "projects":projects,
+    }
+    return render(request, 'index.html', locals())
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'registration/register.html', context)
